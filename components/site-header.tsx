13bb5903926f024a75e691d0d4aa8fc09ecb8b3b
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-09 00:58:04 +0100

    create site header component

---
+++ components/site-header.tsx
@@ -0,0 +1,28 @@
+import { LogIn, PenLine } from "lucide-react"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { getTranslations } from "next-intl/server"
+
+export async function SiteHeader() {
+	const t = await getTranslations("Components.SiteHeader")
+
+	return (
+		<header className="border-b fixed top-0 left-0 right-0 z-50 bg-background">
+			<div className="flex h-16 items-center justify-between px-4 md:px-6">
+				<Link
+					href="/"
+					className="flex items-center gap-2 text-lg font-semibold"
+				>
+					<PenLine className="h-6 w-6" />
+					<span>LINK</span>
+				</Link>
+				<Button variant="ghost" size="sm" className="gap-2" asChild>
+					<Link href="/login">
+						<LogIn className="h-4 w-4" />
+						<span>{t("login")}</span>
+					</Link>
+				</Button>
+			</div>
+		</header>
+	)
+}
