b7b46d70b48b2fc88e9a9302ac99e417808b2a0f
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-09 01:17:40 +0100

    init user in db

--- app/(header)/layout.tsx
+++ app/(header)/layout.tsx
@@ -1,9 +1,24 @@
 import { SiteHeader } from "@/components/site-header"
 import { Button } from "@/components/ui/button"
+import { auth0 } from "@/lib/auth0"
+import prisma from "@/lib/prisma"
 import Link from "next/link"
 import { ReactNode } from "react"
 
 export default async function Layout({ children }: { children: ReactNode }) {
+	const session = await auth0.getSession()
+
+	// pokud se uživatel zrovna zaregistorval, tak si ho potřebuju přidat do vlastní databáze
+	if (session) {
+		const user = await prisma.user.findFirst({
+			where: { email: session.user.email },
+		})
+
+		if (user == null) {
+			await prisma.user.create({ data: { email: session.user.email! } })
+		}
+	}
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<SiteHeader />
