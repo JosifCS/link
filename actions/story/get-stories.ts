4e00f04d8d2ddd585eab9e81da5b10e739abee8c
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-09 01:25:48 +0100

    Update get-stories.ts

--- actions/story/get-stories.ts
+++ actions/story/get-stories.ts
@@ -5,5 +5,7 @@ import prisma from "@/lib/prisma"
 export type GetStoriesQuery = Awaited<ReturnType<typeof getStories>>
 
 export async function getStories(email: string) {
-	return prisma.story.findMany({ where: { createdByEmail: email } })
+	return prisma.story.findMany({
+		where: { createdByEmail: { equals: email } },
+	})
 }
