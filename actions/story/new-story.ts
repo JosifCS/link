36896a8b1e0533852f399a99c413492ca92ae509
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-09 18:50:55 +0100

    save new story uuid to cookies

--- actions/story/new-story.ts
+++ actions/story/new-story.ts
@@ -4,19 +4,26 @@ import { v4 as uuidv4 } from "uuid"
 import prisma from "@/lib/prisma"
 import { redirect } from "next/navigation"
 import { authorize } from "@/modules/auth"
+import { cookies } from "next/headers"
 
 export async function newStory() {
-	const { id } = await authorize(false)
+	const user = await authorize(false)
 
-	const { uuid } = await prisma.story.create({
+	const { uuid, id } = await prisma.story.create({
 		data: {
 			description: "",
 			name: "New story",
 			uuid: uuidv4(),
-			createdById: id,
+			createdById: user.id,
 		},
-		select: { uuid: true },
+		select: { uuid: true, id: true },
 	})
 
-	redirect(`/stories/${uuid}`)
+	// nepřihlášenému uživateli se do cookies na sedm dní uloží uuid právě vytvořeného příběhu
+	if (user.id == null) {
+		const c = await cookies()
+		c.set("story", uuid, { expires: 1000 * 60 * 60 * 25 * 7 })
+	}
+
+	redirect(`/stories/${id}`)
 }
