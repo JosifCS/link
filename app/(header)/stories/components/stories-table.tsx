bd1746493dc954ae7c9866d9a305b9da29f9ee24
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-09 11:27:03 +0100

    story edit button in table

--- app/(header)/stories/components/stories-table.tsx
+++ app/(header)/stories/components/stories-table.tsx
@@ -2,12 +2,28 @@
 
 import { GetStoriesQuery } from "@/actions/story/get-stories"
 import { DataTable } from "@/components/data-table"
+import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
+import { Edit } from "lucide-react"
+import Link from "next/link"
 import { useMemo } from "react"
 
 export function StoriesTable({ data }: { data: GetStoriesQuery }) {
 	const columns = useMemo<ColumnDef<GetStoriesQuery[0], any>[]>(
-		() => [{ accessorKey: "name" }],
+		() => [
+			{ accessorKey: "name", meta: { className: "w-full" } },
+			{
+				accessorKey: "uuid",
+				cell: ({ getValue }) => (
+					<Button variant="ghost" className="h-8 w-8 p-0" asChild>
+						<Link href={`/stories/${getValue()}`}>
+							<span className="sr-only">Edit story</span>
+							<Edit />
+						</Link>
+					</Button>
+				),
+			},
+		],
 		[]
 	)
 
