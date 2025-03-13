20694f0c99c03c31b3c67ef2534dc517b4f3d1f8
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-13 19:21:54 +0100

    back buttons

--- app/(header)/stories/[storyId]/chapters/[chapterId]/page.tsx
+++ app/(header)/stories/[storyId]/chapters/[chapterId]/page.tsx
@@ -2,11 +2,16 @@ import { PageProps } from "@/types/global"
 import { ChapterInfo } from "./components/chapter-info"
 import prisma from "@/lib/prisma"
 import { notFound } from "next/navigation"
+import { getTranslations } from "next-intl/server"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { ChevronLeft } from "lucide-react"
 
 export default async function Page({
 	params,
 }: PageProps<"storyId" | "chapterId">) {
 	const { chapterId, storyId } = await params
+	const t = await getTranslations("Stories.Story.Chapters.Chapter")
 
 	const chapter = await prisma.chapter.findFirst({
 		where: { id: +chapterId },
@@ -18,7 +23,12 @@ export default async function Page({
 		<div className="container mx-auto py-6 space-y-6">
 			<div className="flex items-center justify-between">
 				<div className="flex items-center gap-2">
-					<h1 className="text-2xl font-bold">{"Kapitola"}</h1>
+					<Button variant="outline" size="sm" asChild>
+						<Link href={`/stories/${storyId}/chapters`}>
+							<ChevronLeft className="h-4 w-4" />
+						</Link>
+					</Button>
+					<h1 className="text-2xl font-bold">{t("chapter")}</h1>
 				</div>
 			</div>
 
--- app/(header)/stories/[storyId]/characters/[characterId]/page.tsx
+++ app/(header)/stories/[storyId]/characters/[characterId]/page.tsx
@@ -2,11 +2,16 @@ import prisma from "@/lib/prisma"
 import { PageProps } from "@/types/global"
 import { notFound } from "next/navigation"
 import { CharacterInfo } from "./components/character-info"
+import { getTranslations } from "next-intl/server"
+import { Button } from "@/components/ui/button"
+import { ChevronLeft } from "lucide-react"
+import Link from "next/link"
 
 export default async function Page({
 	params,
 }: PageProps<"storyId" | "characterId">) {
 	const { storyId, characterId } = await params
+	const t = await getTranslations("Stories.Story.Characters.Character")
 
 	const character = await prisma.character.findFirst({
 		where: { id: +characterId },
@@ -18,7 +23,12 @@ export default async function Page({
 		<div className="container mx-auto py-6 space-y-6">
 			<div className="flex items-center justify-between">
 				<div className="flex items-center gap-2">
-					<h1 className="text-2xl font-bold">{"Postava"}</h1>
+					<Button variant="outline" size="sm" asChild>
+						<Link href={`/stories/${storyId}/characters`}>
+							<ChevronLeft className="h-4 w-4" />
+						</Link>
+					</Button>
+					<h1 className="text-2xl font-bold">{t("character")}</h1>
 				</div>
 			</div>
 
