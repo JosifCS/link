861e51f901fc309e629918f384c91e38ed1d7642
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-13 19:12:13 +0100

    fix chpter-info

--- app/(header)/stories/[storyId]/chapters/[chapterId]/components/chapter-info.tsx
+++ app/(header)/stories/[storyId]/chapters/[chapterId]/components/chapter-info.tsx
@@ -1,15 +1,9 @@
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card"
 import { getTranslations } from "next-intl/server"
 import prisma from "@/lib/prisma"
 import { notFound } from "next/navigation"
 import { Skeleton } from "@/components/ui/skeleton"
 import { ChapterForm } from "./chapter-form"
+import { Card } from "@/components/card"
 
 type ChapterInfoProps = {
 	chapterId: number
@@ -27,35 +21,19 @@ export async function ChapterInfo({ chapterId }: ChapterInfoProps) {
 	if (chapter == null) return notFound()
 
 	return (
-		<Card>
-			<CardHeader className="flex flex-row items-center justify-between">
-				<div className="space-y-1.5">
-					<CardTitle>{t("title")}</CardTitle>
-					<CardDescription>
-						{t("subTitle", { story: "XXX" })}
-					</CardDescription>
-				</div>
-			</CardHeader>
-			<CardContent className="space-y-4">
-				<ChapterForm
-					t={{ name: t("name"), description: t("description") }}
-					value={chapter}
-				/>
-			</CardContent>
+		<Card title={t("title")}>
+			<ChapterForm
+				t={{ name: t("name"), description: t("description") }}
+				value={chapter}
+			/>
 		</Card>
 	)
 }
 
 export function ChapterInfoSkeleton() {
 	return (
-		<Card>
-			<CardHeader>
-				<Skeleton className="h-6 w-44" />
-				<Skeleton className="h-5 w-52" />
-			</CardHeader>
-			<CardContent className="space-y-4">
-				<Skeleton className="w-full h-52" />
-			</CardContent>
+		<Card titleSkeleton>
+			<Skeleton className="w-full h-52" />
 		</Card>
 	)
 }
