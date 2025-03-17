cea08a33040c7d75ada3586ee5c711571660ad06
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-17 19:34:46 +0100

    Create dialogs-list.tsx

---
+++ app/(header)/stories/[storyId]/chapters/[chapterId]/components/dialogs-list.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import { GetDialogsQuery } from "@/actions/chapter/get-dialogs"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useChapterState } from "@/states/chapter-state"
+import { Plus, Search } from "lucide-react"
+import { useMemo, useState } from "react"
+
+export type DialogListProps = {
+	dialogs: GetDialogsQuery
+	t: Record<"placeholder" | "new" | "noDialogs" | "sentences", string>
+}
+
+export function DialogsList({ dialogs, t }: DialogListProps) {
+	const [search, setSearch] = useState<string>("")
+	const { setDialog } = useChapterState()
+
+	const filteredDialogs = useMemo<GetDialogsQuery>(() => {
+		if (search.length) {
+			const stl = search.toLowerCase()
+			return dialogs.filter((x) => x.name.toLowerCase().includes(stl))
+		}
+		return dialogs
+	}, [dialogs, search])
+
+	return (
+		<div className="flex flex-col grow">
+			<div className="flex gap-2 flex-row">
+				<div className="relative grow">
+					<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+					<Input
+						type="search"
+						placeholder={t.placeholder}
+						className="pl-8"
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+				</div>
+				<Button type="button" className="px-3" title={t.new}>
+					<Plus />
+				</Button>
+			</div>
+
+			{filteredDialogs.length == 0 ? (
+				<div className="text-muted-foreground text-sm text-center mt-auto mb-auto">
+					{t.noDialogs}
+				</div>
+			) : (
+				<div className="space-y-2 flex-1 overflow-auto">
+					{filteredDialogs.map((dialog) => (
+						<div
+							key={dialog.id}
+							className="p-3 rounded-md cursor-pointer hover:bg-muted"
+							onClick={() => setDialog(dialog.id)}
+						>
+							<h3 className="font-medium">{dialog.name}</h3>
+							<div className="text-sm text-muted-foreground">
+								<span>
+									{dialog._count.sentences} {t.sentences}
+								</span>
+								<span className="mx-1">•</span>
+								<span>{dialog.character.name}</span>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	)
+}
