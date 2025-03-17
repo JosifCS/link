1a476947a4a00295542c5754755480eb60c6ff92
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-03-17 19:00:56 +0100

    Create chapter-state.tsx

---
+++ states/chapter-state.tsx
@@ -0,0 +1,20 @@
+import { create } from "zustand"
+
+type ChapterState = {
+	dialogId: number | null
+	sentenceId: number | null
+	level: () => "chapter" | "dialog" | "sentence"
+	setChapter: () => void
+	setDialog: (diloagId: number) => void
+	setSentence: (sentenceId: number) => void
+}
+
+export const useChapterState = create<ChapterState>()((set, get) => ({
+	dialogId: null,
+	sentenceId: null,
+	level: () =>
+		get().sentenceId ? "sentence" : get().dialogId ? "dialog" : "chapter",
+	setChapter: () => set((state) => ({ dialogId: null, sentenceId: null })),
+	setDialog: (dialogId) => set((state) => ({ dialogId, sentenceId: null })),
+	setSentence: (sentenceId) => set((state) => ({ sentenceId })),
+}))
